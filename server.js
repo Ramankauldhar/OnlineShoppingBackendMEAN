720d456fff1ba589bb4b2d8b2b258c9e6c32f4b2
RamanKauldhar <rmnkaul979697@gmali.com>
2023-06-05 09:48:06 -0400

    V1.1:Connected to Mongo DB

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Online Shopping");
 });
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/mydatabase", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+console.log("DB connected");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Start the server
 const port = 9090;
